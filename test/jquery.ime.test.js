9facd45f3a50a9baee5d9bcbe1fa67debfc88c67
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2012-10-14 15:10:35 +0200

    More initialization tests

--- test/jquery.ime.test.js
+++ test/jquery.ime.test.js
@@ -1,7 +1,7 @@
 ( function ( $ ) {
 	'use strict';
 
-	var $textarea, $input;
+	var $textarea, $input, textareaIME, inputIME;
 
 	QUnit.module( 'jquery.ime - $.fn.ime Tests', {
 		setup: function () {
@@ -12,11 +12,20 @@
 		}
 	} );
 
-	QUnit.test( 'Initialization', 3, function ( assert ) {
+	QUnit.test( 'Initialization tests', 7, function ( assert ) {
 		assert.strictEqual( typeof $textarea.ime, 'function', 'ime function exists' );
 		assert.strictEqual( typeof $textarea.data('ime'), 'undefined', 'ime not initialized before calling ime()' );
 
+		$input.ime();
+		inputIME = $input.data( 'ime' );
+		assert.strictEqual( inputIME.active, false, 'ime is initially inactive' );
+		assert.strictEqual( inputIME.context, '', 'context is initially empty' );
+		assert.strictEqual( inputIME.inputmethod, null, 'inputmethod is initially null' );
+		assert.strictEqual( inputIME.options.imePath, '../', 'imePath is "../" by default' );
+		// TODO test selector
+
 		$textarea.ime();
-		assert.strictEqual( typeof $textarea.data('ime'), 'object', 'ime initialized after calling ime()' );
+		textareaIME = $textarea.data( 'ime' );
+		assert.strictEqual( typeof textareaIME, 'object', '<textarea> ime initialized after calling ime()' );
 	} );
 }( jQuery ) );
