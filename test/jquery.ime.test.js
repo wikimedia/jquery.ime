20f5a262b4d2a977d9e71a9375d86fdd8c86ec6e
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2012-10-16 16:07:06 +0200

    Selector tests

--- test/jquery.ime.test.js
+++ test/jquery.ime.test.js
@@ -1,7 +1,8 @@
 ( function ( $ ) {
 	'use strict';
 
-	var $textarea, $input, textareaIME, inputIME;
+	var $textarea, textareaIME,
+		$input, inputIME;
 
 	QUnit.module( 'jquery.ime - $.fn.ime Tests', {
 		setup: function () {
@@ -12,7 +13,7 @@
 		}
 	} );
 
-	QUnit.test( 'Initialization tests', 8, function ( assert ) {
+	QUnit.test( 'Initialization tests', 7, function ( assert ) {
 		assert.strictEqual( typeof $textarea.ime, 'function', 'ime function exists' );
 		assert.strictEqual( typeof $textarea.data('ime'), 'undefined', 'ime not initialized before calling ime()' );
 
@@ -22,10 +23,21 @@
 		assert.strictEqual( inputIME.context, '', 'context is initially empty' );
 		assert.strictEqual( inputIME.inputmethod, null, 'inputmethod is initially null' );
 		assert.strictEqual( inputIME.options.imePath, '../', 'imePath is "../" by default' );
-		assert.strictEqual( typeof inputIME.selector, 'object', 'selector is defined' );
 
+		var $specialPath = $( '<textarea>' ),
+			specialPath = '../test';
+		$specialPath.ime( { imePath: specialPath } );
+		assert.strictEqual( $specialPath.data( 'ime' ).options.imePath, specialPath,
+							'imePath is defined correctly using options in the constructor' );
+	} );
+
+	QUnit.test( 'Selector tests', 3, function ( assert ) {
 		$textarea.ime();
 		textareaIME = $textarea.data( 'ime' );
-		assert.strictEqual( typeof textareaIME, 'object', '<textarea> ime initialized after calling ime()' );
+		var selector = textareaIME.selector.data( 'imeselector' );
+		assert.strictEqual( typeof selector, 'object', 'selector on textarea is defined' );
+
+		assert.strictEqual( selector.active, false, 'selector is not active initially' );
+		assert.strictEqual( selector.inputmethod, null, 'inputmethod is not enabled initially' );
 	} );
 }( jQuery ) );
