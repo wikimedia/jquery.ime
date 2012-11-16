9b6a6b4417ae45d8c1246db28c134aeb4964eb68
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2012-11-16 07:30:31 +0200

    Removed unneeded start() and stop().

--- test/jquery.ime.test.js
+++ test/jquery.ime.test.js
@@ -99,19 +99,13 @@
 		// set it again
 		$.ime.preferences.setLanguage( 'hi' );
 		assert.strictEqual( $.ime.preferences.getPreviousLanguages().length, 1, 'Hindi not duplicated in previous languages' );
-		stop();
-		start();
 		$.ime.preferences.setLanguage( 'kn' );
 		$.ime.preferences.setIM( 'kn-inscript' );
 		assert.strictEqual( $.ime.preferences.getPreviousLanguages().length, 2, 'Kannada added to previous languages' );
-		stop();
-		start();
 		$.ime.preferences.setLanguage( 'hi' );
 		$.ime.preferences.setIM( 'hi-inscript' );
 		assert.strictEqual( $.ime.preferences.getIM('hi'), 'hi-inscript', 'Hindi Inscript is the preferred IM for Hindi' );
 		assert.strictEqual( $.ime.preferences.getIM('kn'), 'kn-inscript', 'Kannada Inscript is the preferred IM for Kannada' );
-		stop();
-		start();
 	} );
 
 	QUnit.module( 'jquery.ime - input method rules tests', {
