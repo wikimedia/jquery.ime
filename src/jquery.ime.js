6dc08ec285bdda6cb71482edd6d95270bed1b18e
Prageck <prageck@gmail.com>
2013-05-06 18:42:41 +0530

    Fixed the remaining boundary case for bug 43904 on IE8

--- src/jquery.ime.js
+++ src/jquery.ime.js
@@ -339,6 +339,7 @@
 			if ( range && range.parentElement() === el ) {
 				len = el.value.length;
 				normalizedValue = el.value.replace( /\r\n/g, '\n' );
+				newLines = normalizedValue.match( /\n/g );
 
 				// Create a working TextRange that lives only in the input
 				textInputRange = el.createTextRange();
@@ -351,26 +352,20 @@
 				endRange.collapse( false );
 
 				if ( textInputRange.compareEndPoints( 'StartToEnd', endRange ) > -1 ) {
-					start = end = len;
+					if ( newLines ) {
+						start = end = len - newLines.length;
+					} else {
+						start = end = len;
+					}
 				} else {
 					start = -textInputRange.moveStart( 'character', -len );
-					start += normalizedValue.slice( 0, start ).split( '\n' ).length - 1;
 
 					if ( textInputRange.compareEndPoints( 'EndToEnd', endRange ) > -1 ) {
 						end = len;
 					} else {
 						end = -textInputRange.moveEnd( 'character', -len );
-						end += normalizedValue.slice( 0, end ).split( '\n' ).length - 1;
 					}
 				}
-			}
-			
-			// only find newlines till current caret position and update
-			// start and end caret positions accordingly
-			newLines = normalizedValue.substring( 0, start ).match( /\n/g );
-			if ( newLines ) {
-				start -= newLines.length;
-				end -= newLines.length;
 			}
 		}
 
