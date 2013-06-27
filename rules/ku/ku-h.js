f7c7f4029ad718d1c92e9dc1dde0eb70e15874ec
ghybu <ghybu.w@gmail.com>
2013-06-27 11:50:18 +0200

    Add ku-h.js and ku-tr.js
    Add 2 input method for Latin Kurdish alphabet.

---
+++ rules/ku/ku-h.js
@@ -0,0 +1,61 @@
+﻿( function ( $ ) {
+	'use strict';
+
+	var kuH = {
+		id: 'ku-h',
+		name: 'Kurdish h',
+		description: 'writing Kurdish-letters adding h\'s',
+		date: '2013-06-26',
+		URL: 'http://github.com/wikimedia/jquery.ime',
+		author: 'Ghybu',
+		license: 'GPLv3',
+		version: '1.0',
+		patterns: [
+			['çh', 'ch'],
+			['şh', 'sh'],
+			['ḧh', 'hh'],
+			['ẍh', 'xh'],
+			['êe', 'ee'],
+			['îi', 'ii'],
+			['ûu', 'uu'],
+			['Çh', 'Ch'],
+			['Şh', 'Sh'],
+			['Ḧh', 'Hh'],
+			['Ẍh', 'Xh'],
+			['Êe', 'Ee'],
+			['Îi', 'Ii'],
+			['Ûu', 'Uu'],
+			['ÇH', 'CH'],
+			['ŞH', 'SH'],
+			['ḦH', 'HH'],
+			['ẌH', 'XH'],
+			['ÊE', 'EE'],
+			['ÎI', 'II'],
+			['ÛU', 'UU'],
+			
+			['ch', 'ç'],
+			['sh', 'ş'],
+			['hh', 'ḧ'],
+			['xh', 'ẍ'],
+			['ee', 'ê'],
+			['ii', 'î'],
+			['uu', 'û'],
+			['Ch', 'Ç'],
+			['Sh', 'Ş'],
+			['Hh', 'Ḧ'],
+			['Xh', 'Ẍ'],
+			['Ee', 'Ê'],
+			['Ii', 'Î'],
+			['Uu', 'Û'],
+			['CH', 'Ç'],
+			['SH', 'Ş'],
+			['HH', 'Ḧ'],
+			['XH', 'Ẍ'],
+			['EE', 'Ê'],
+			['II', 'Î'],
+			['UU', 'Û']
+	};
+
+	$.ime.register( kuH );
+
+}( jQuery ) );
---
+++ rules/ku/ku-tr.js
@@ -0,0 +1,47 @@
+﻿( function ( $ ) {
+	'use strict';
+
+	var kuTr = {
+		id: 'ku-tr',
+		name: 'Kurdish tr',
+		description: 'writing Kurdish-letters using the TR keyboard',
+		date: '2013-06-26',
+		URL: 'http://github.com/wikimedia/jquery.ime',
+		author: 'Ghybu',
+		license: 'GPLv3',
+		version: '1.0',
+		patterns: [
+			['ẍẍ', 'ğ'],
+			['ii', 'ı'],
+			['îî', 'i'],
+			['êê', 'ö'],
+			['ûû', 'ü'],
+ 			['ḧh', 'hh'],
+			['Ẍẍ', 'Ğ'],
+			['Îî', 'İ'],
+			['Êê', 'Ö'],
+			['Ûû', 'Ü'],
+			['Ḧh', 'Hh'],
+			['ẌẌ', 'Ğ'],
+			['ÎÎ', 'İ'],
+			['ÊÊ', 'Ö'],
+			['ÛÛ', 'Ü'],
+			['ḦH', 'HH'],
+
+			['ğ', 'ẍ'],
+			['ı', 'i'],
+			['i', 'î'],
+			['ö', 'ê'],
+			['ü', 'û'],
+ 			['hh', 'ḧ'],
+			['Ğ', 'Ẍ'],
+			['İ', 'Î'],
+			['Ö', 'Ê'],
+			['Ü', 'Û'],
+			['Hh', 'Ḧ'],
+			['HH', 'Ḧ']
+	};
+
+	$.ime.register( kuTr );
+
+}( jQuery ) );
