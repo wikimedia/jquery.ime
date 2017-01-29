5911cb104040a8f3944f7f746eb5a43bd96294ca
John Erling Blad <jeblad@gmail.com>
2017-01-29 03:35:00 +0100

    Changed the rules, first approximation

--- rules/nb/nb-normforms.js
+++ rules/nb/nb-normforms.js
@@ -7,36 +7,36 @@
 		description: 'Norwegian input method with most common form transliterated',
 		date: '2012-12-04',
 		URL: 'http://www.evertype.com/alphabets/bokmaal-norwegian.pdf',
-		// URL: 'http://www.evertype.com/alphabets/nynorsk-norwegian.pdf',
 		author: 'John Erling Blad',
 		license: 'GPLv3',
 		version: '1.0',
 		contextLength: 1,
 		maxKeyLength: 3,
 		patterns: [
-			// The most common transliterations
+
 			[ 'aa', 'å' ],
-			[ 'AA', 'Å' ],
-			[ 'Aa', 'Å' ],
+			[ '(AA|Aa|aA)', 'Å' ],
 			[ 'ae', 'æ' ],
-			[ 'AE', 'Æ' ],
-			[ 'Ae', 'Æ' ],
+			[ '(AE|Ae|aE)', 'Æ' ],
 			[ 'oe', 'ø' ],
-			[ 'OE', 'Ø' ],
-			[ 'Oe', 'Ø' ],
-			// The previous as negated transliterations, mostly for names
-			[ 'åa', 'a', 'aa' ],
-			[ 'ÅA', 'A', 'AA' ],
-			[ 'Åa', 'A', 'Aa' ],
-			[ 'åA', 'a', 'aA' ],
-			[ 'æe', 'e', 'ae' ],
-			[ 'ÆE', 'E', 'AE' ],
-			[ 'Æe', 'E', 'Ae' ],
-			[ 'æE', 'e', 'aE' ],
-			[ 'øe', 'e', 'oe' ],
-			[ 'ØE', 'E', 'OE' ],
-			[ 'Øe', 'E', 'Oe' ], // this fails for some names like "Øen"
-			[ 'øE', 'e', 'oE' ]
+			[ '(OE|Oe|oE)', 'Ø' ],
+
+			[ '([#0-9a-fA-F])å([0-9a-fA-F])', '[aA]', '$1aa$2' ],
+			[ '([#0-9a-fA-F])Å([0-9a-fA-F])', '[aA]', '$1AA$2' ],
+			[ 'å([aA])', '[aA]', 'a$1' ],
+			[ 'Å([aA])', '[aA]', 'A$1' ],
+
+			[ '([#0-9a-fA-F])æ([0-9a-fA-F])', '[eE]', '$1ae$2' ],
+			[ '([#0-9a-fA-F])Æ([0-9a-fA-F])', '[eE]', '$1AE$2' ],
+			[ 'æ([eE])', '[eE]', 'a$1' ],
+			[ 'Æ([eE])', '[eE]', 'A$1' ],
+
+			[ '([bBdDeEgGiIkKlLmMnNpPrRsStTuUvV])ø([nNrR\s\W])', '[eE]', '$1oe$2' ],
+			[ '([bBdDeEgGiIkKlLmMnNpPrRsStTuUvV])Ø([nNrR\s\W])', '[eE]', '$1OE$2' ],
+			[ '([rR])ø([kK\s\W])', '[eE]', '$1oe$2' ],
+			[ '([rR])Ø([kK\s\W])', '[eE]', '$1OE$2' ],
+			[ 'ø([eE])', '[eE]', 'o$1' ],
+			[ 'Ø([eE])', '[eE]', 'O$1' ]
 			// historically similar forms
 			// "Å" is sometimes written as "Aa", and "å" as "aa", but in names
 			// it is not generally acceptable to use this transliteration. To
