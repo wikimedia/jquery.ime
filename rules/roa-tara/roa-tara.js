e3943d5bae441dfd6a7929b6ea3bb783cb74a2d7
Amire80 <amir.aharoni@mail.huji.ac.il>
2017-06-02 07:55:21 +0300

    Fix indentation in roa-tara.js

--- rules/roa-tara/roa-tara.js
+++ rules/roa-tara/roa-tara.js
@@ -1,24 +1,24 @@
 ( function ( $ ) {
-'use strict';
+	'use strict';
 
-var roaTara = {
-id: 'roa-tara-GVU',
-name: 'Tarandine',
-description: 'Tarandine input method.',
-date: '2013-09-01',
-URL: 'http://github.com/wikimedia/jquery.ime',
-author: 'roa-tara.wiki community',
-license: 'GPLv3',
-version: '1.0',
-contextLength: 0,
-maxKeyLength: 2,
-patterns: [
-	['a§', 'á'],
-	['o§', 'ó'],
-	['A§', 'Á'],
-	['O§', 'Ó']
-]
-};
+	var roaTara = {
+		id: 'roa-tara-GVU',
+		name: 'Tarandine',
+		description: 'Tarandine input method.',
+		date: '2013-09-01',
+		URL: 'http://github.com/wikimedia/jquery.ime',
+		author: 'roa-tara.wiki community',
+		license: 'GPLv3',
+		version: '1.0',
+		contextLength: 0,
+		maxKeyLength: 2,
+		patterns: [
+			['a§', 'á'],
+			['o§', 'ó'],
+			['A§', 'Á'],
+			['O§', 'Ó']
+		]
+	};
 
-$.ime.register( roaTara );
+	$.ime.register( roaTara );
 }( jQuery ) );
