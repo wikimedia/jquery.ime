ce5a04141f6f49cdf82745cd510f251174b891a7
Mahuton <bpossoupe@example.com>
2018-05-19 12:51:57 +0200

    updaing keys and test file

--- rules/fon/fon-alt.js
+++ rules/fon/fon-alt.js
@@ -5,17 +5,18 @@
 	var fonAlt = {
 		id: 'fon-alt',
 		name: 'fon-alt',
-		description: 'fon input keyboard',
+		description: 'Fon input keyboard',
 		date: '2018-05-18',
 		URL: 'http://github.com/wikimedia/jquery.ime',
 		author: 'Mahuton POSSOUPE',
 		license: 'GPLv3',
 		version: '1.0',
 		patterns: [
-      			[ '~E', 'Ɛ' ],
+			[ '~E', 'Ɛ' ],
 			[ '~O', 'Ɔ' ],
+			[ '~o', 'ɔ' ],
 			[ '~D', 'Ɖ' ],
-      			[ '~d', 'ɖ' ]
+			[ '~d', 'ɖ' ]
 		]
 	};
 
