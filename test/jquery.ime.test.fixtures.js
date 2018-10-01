fa084363d6723912f5360a07cbe0e5d8564be5e5
Jnanaranjan Sahu (ଜ୍ଞାନରଞ୍ଜନ ସାହୁ) <gyana111@gmail.com>
2018-10-01 10:16:30 +0530

    Fixed Commas
    Updated code after adding commas

--- test/jquery.ime.test.fixtures.js
+++ test/jquery.ime.test.fixtures.js
@@ -3942,8 +3942,8 @@ var palochkaVariants = {
 			{ input: 'a', output: 'ᱟ', description: 'Santali a -> ᱟ' },
 			{ input: 'b', output: 'ᱵ', description: 'Santali b -> ᱵ' },
 			{ input: 'P', output: 'ᱯ', description: 'Santali P -> ᱯ' },
-			{ input: 'Q', output: 'ᱻ', description: 'Santali Q -> ᱻ' }
-			{ input: 'W', output: 'ᱣ', description: 'Santali W -> ᱣ' }
+			{ input: 'Q', output: 'ᱻ', description: 'Santali Q -> ᱻ' },
+			{ input: 'W', output: 'ᱣ', description: 'Santali W -> ᱣ' },
 			{ input: 'c', output: 'ᱪ', description: 'Santali c -> ᱪ' }
 		],
 		inputmethod: 'sat-sarjom-baha'
