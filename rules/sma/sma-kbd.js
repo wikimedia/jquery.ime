f6f2615d773e6598881021bee3b5ee27fc46ac2a
jhsoby <jhsoby@gmail.com>
2018-10-04 13:58:45 +0200

    Spaces to tabs²
    Spaces to tabs as indentation for consistency.

--- rules/sma/sma-kbd.js
+++ rules/sma/sma-kbd.js
@@ -1,25 +1,25 @@
 ( function ( $ ) {
-  'use strict';
+	'use strict';
 
-  var defs = {
-    id: 'sma-kbd',
-    name: 'Åarjesaemien',
-    description: 'Southern Sami keyboard layout',
-    date: '2018-08-08',
-    URL: 'http://divvun.no/keyboards/layout-sma.html',
-    author: 'Jon Harald Søby',
-    license: 'GPLv3',
-    version: '1.0',
-    patterns: [
-      [ '¨', 'ï' ],
-      [ '\^', 'Ï' ],
-      [ 'ø', 'ö' ],
-      [ 'Ø', 'Ö' ],
-      [ 'ä', 'æ' ],
-      [ 'Ä', 'Æ' ]
-    ]
+	var defs = {
+		id: 'sma-kbd',
+		name: 'Åarjesaemien',
+		description: 'Southern Sami keyboard layout',
+		date: '2018-08-08',
+		URL: 'http://divvun.no/keyboards/layout-sma.html',
+		author: 'Jon Harald Søby',
+		license: 'GPLv3',
+		version: '1.0',
+		patterns: [
+			[ '¨', 'ï' ],
+			[ '\^', 'Ï' ],
+			[ 'ø', 'ö' ],
+			[ 'Ø', 'Ö' ],
+			[ 'ä', 'æ' ],
+			[ 'Ä', 'Æ' ]
+		]
 
-  };
-  $.ime.register( defs );
+	};
+	$.ime.register( defs );
 
 }( jQuery ) );
--- rules/smn/smn-kbd.js
+++ rules/smn/smn-kbd.js
@@ -1,45 +1,45 @@
 ( function ( $ ) {
-  'use strict';
+	'use strict';
 
-  var defs = {
-    id: 'smn-kbd',
-    name: 'Anarâškielâ',
-    description: 'Inari Sami keyboard layout',
-    date: '2018-08-08',
-    URL: 'http://divvun.no/keyboards/layout-smn.html',
-    author: 'Jon Harald Søby',
-    license: 'GPLv3',
-    version: '1.0',
-    maxKeyLength: 2,
-    patterns: [
-      [ 'q', 'á' ],
-      [ 'Q', 'Á' ],
-      [ 'w', 'š' ],
-      [ 'W', 'Š' ],
-      [ 'å', 'â' ],
-      [ 'Å', 'Â' ],
-      [ 'ö', 'đ' ],
-      [ 'Ö', 'Đ' ],
-      [ '<', 'ž' ],
-      [ '>', 'Ž' ],
-      [ 'x', 'č' ],
-      [ 'X', 'Č' ],
-      // Tilde negations to get the "normal" letters
-      [ '~á', 'q' ],
-      [ '~Á', 'Q' ],
-      [ '~š', 'w' ],
-      [ '~Š', 'W' ],
-      [ '~â', 'å' ],
-      [ '~Â', 'Å' ],
-      [ '~đ', 'ö' ],
-      [ '~Đ', 'Ö' ],
-      [ '~ž', '<' ],
-      [ '~Ž', '>' ],
-      [ '~č', 'x' ],
-      [ '~C', 'X']
-    ]
+	var defs = {
+		id: 'smn-kbd',
+		name: 'Anarâškielâ',
+		description: 'Inari Sami keyboard layout',
+		date: '2018-08-08',
+		URL: 'http://divvun.no/keyboards/layout-smn.html',
+		author: 'Jon Harald Søby',
+		license: 'GPLv3',
+		version: '1.0',
+		maxKeyLength: 2,
+		patterns: [
+			[ 'q', 'á' ],
+			[ 'Q', 'Á' ],
+			[ 'w', 'š' ],
+			[ 'W', 'Š' ],
+			[ 'å', 'â' ],
+			[ 'Å', 'Â' ],
+			[ 'ö', 'đ' ],
+			[ 'Ö', 'Đ' ],
+			[ '<', 'ž' ],
+			[ '>', 'Ž' ],
+			[ 'x', 'č' ],
+			[ 'X', 'Č' ],
+			// Tilde negations to get the "normal" letters
+			[ '~á', 'q' ],
+			[ '~Á', 'Q' ],
+			[ '~š', 'w' ],
+			[ '~Š', 'W' ],
+			[ '~â', 'å' ],
+			[ '~Â', 'Å' ],
+			[ '~đ', 'ö' ],
+			[ '~Đ', 'Ö' ],
+			[ '~ž', '<' ],
+			[ '~Ž', '>' ],
+			[ '~č', 'x' ],
+			[ '~C', 'X']
+		]
 
-  };
-  $.ime.register( defs );
+	};
+	$.ime.register( defs );
 
 }( jQuery ) );
