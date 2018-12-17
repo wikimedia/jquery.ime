085eeef07673fbb5cdf76428f48d6efd677b4749
Amir Aharoni <amir.aharoni@mail.huji.ac.il>
2018-12-17 22:50:51 +0200

    Add eslint and stylelint
    Many rules are disabled. They will be gradually enabled.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,15 @@
-'use strict';
-
-/* jshint node: true */
+/* eslint-env node */
 module.exports = function ( grunt ) {
+	'use strict';
+
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
 	grunt.loadNpmTasks( 'grunt-contrib-concat' );
 	grunt.loadNpmTasks( 'grunt-contrib-copy' );
-	grunt.loadNpmTasks( 'grunt-contrib-csslint' );
-	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-jscs' );
-	// Project configuration.
+
 	grunt.initConfig( {
 		pkg: grunt.file.readJSON( 'package.json' ),
 		meta: {
@@ -65,45 +63,32 @@ module.exports = function ( grunt ) {
 		qunit: {
 			files: [ 'test/index.html' ]
 		},
-		jshint: {
-			options: {
-				jshintrc: true
-			},
+		eslint: {
 			all: [
-				'*.js',
-				'src/*.js',
-				'rules/**/*.js',
-				'test/**/*.js'
+				'**/*.js',
+				'!libs/**',
+				'!dist/**',
+				'!node_modules/**'
 			]
 		},
-		jscs: {
-			fix: {
-				options: {
-					fix: true
-				},
-				src: '<%= jshint.all %>'
+		stylelint: {
+			options: {
+				syntax: 'css'
 			},
-			main: {
-				src: '<%= jshint.all %>'
-			}
-		},
-		csslint: {
-			all: [
-				'css/**/*.css'
+			src: [
+				'**/*.css',
+				'!node_modules/**'
 			]
 		},
 		watch: {
 			files: [
-				'.{csslintrc,jscsrc,jshintignore,jshintrc}',
-				'<%= jshint.all %>',
-				'<%= csslint.all %>'
+				'.{eslintrc.json}'
 			],
 			tasks: 'lint'
 		}
 	} );
 
-	// Default task.
-	grunt.registerTask( 'lint', [ 'jshint', 'jscs:main', 'csslint' ] );
+	grunt.registerTask( 'lint', [ 'eslint', 'stylelint' ] );
 	grunt.registerTask( 'build', [ 'concat', 'uglify', 'copy' ] );
 	grunt.registerTask( 'test', [ 'build', 'qunit' ] );
 	grunt.registerTask( 'default', [ 'lint', 'test' ] );
