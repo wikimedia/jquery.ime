d0af7ae8c2c7b1a4c4a58f94f6ac2137a8dc2451
Amir Aharoni <amir.aharoni@mail.huji.ac.il>
2021-05-15 20:44:31 +0300

    Fix no-multi-spaces eslint errors
    No functional changes.
    
    Most of these were in tests, next to "output" and
    "description", and were completely useless, and many
    others were in IPA and Hebrew files, which I wrote myself many
    years ago, and there were never really useful.
    
    The exception for this rule is now removed from .eslintrc.json.

--- rules/fonipa/ipa-sil.js
+++ rules/fonipa/ipa-sil.js
@@ -67,7 +67,7 @@
 
 			[ 'p=', 'ʘ' ],
 			[ 'ǃ<', 'ǀ' ], // This is not an exclamation mark
-			[ '!',  'ǃ' ],
+			[ '!', 'ǃ' ],
 			[ 'ǃ=', 'ǂ' ], // This is not an exclamation mark
 			[ 'ǃ>', 'ǁ' ], // This is not an exclamation mark
 
@@ -112,20 +112,20 @@
 			[ 'O>', 'ɤ' ],
 			[ 'u>', 'ʌ' ],
 			[ 'a=', 'ɑ' ],
-			[ 'u',  'u' ],
+			[ 'u', 'u' ],
 			[ 'u<', 'ʊ' ],
 			[ 'o<', 'ɔ' ],
 			[ 'o=', 'ɒ' ],
 
-			[ 'ˈ}',        'ˌ' ], // }}
-			[ '}',         'ˈ' ],
-			[ 'ː:',        'ˑ' ], // ::
-			[ 'ˑ:',        'ːː' ], // ::: // Not IPA sanctioned
-			[ ':',         'ː' ],
+			[ 'ˈ}', 'ˌ' ], // }}
+			[ '}', 'ˈ' ],
+			[ 'ː:', 'ˑ' ], // ::
+			[ 'ˑ:', 'ːː' ], // ::: // Not IPA sanctioned
+			[ ':', 'ː' ],
 			[ '\\*\\*\\*', '\u0306' ], // Combining breve
-			[ '\\.<',      '|' ],
-			[ '\\.=',      '‖' ],
-			[ '#=',        '‿' ],
+			[ '\\.<', '|' ],
+			[ '\\.=', '‖' ],
+			[ '#=', '‿' ],
 
 			// Tones
 			[ '([˥-˩])4', '$1˥' ],
@@ -138,13 +138,13 @@
 			[ '#2', '˧' ],
 			[ '#1', '˨' ],
 			[ '#0', '˩' ],
-			[ '(?:\u0300)3', '\u030C' ], // @13  - Combining caron
-			[ '(?:\u0301)1', '\u0302' ], // @31  - Combining circumflex accent
-			[ '(?:\u0304)3', '\u1dc4' ], // @23  - Combining macron-acute
-			[ '(?:\u0300)2', '\u1dc5' ], // @12  - Combining grave-macron
+			[ '(?:\u0300)3', '\u030C' ], // @13 - Combining caron
+			[ '(?:\u0301)1', '\u0302' ], // @31 - Combining circumflex accent
+			[ '(?:\u0304)3', '\u1dc4' ], // @23 - Combining macron-acute
+			[ '(?:\u0300)2', '\u1dc5' ], // @12 - Combining grave-macron
 			[ '(?:\u030C)1', '\u1dc8' ], // @131 - Combining grave-acute-grave
-			[ '(?:\u0304)1', '\u1dc6' ], // @21  - Combining macron-grave // Not IPA sanctioned
-			[ '(?:\u0301)2', '\u1dc7' ], // @32  - Combining acute-macron // Not IPA sanctioned
+			[ '(?:\u0304)1', '\u1dc6' ], // @21 - Combining macron-grave // Not IPA sanctioned
+			[ '(?:\u0301)2', '\u1dc7' ], // @32 - Combining acute-macron // Not IPA sanctioned
 			[ '(?:\u0302)3', '\u1dc9' ], // @313 - Combining acute-grave-acute // Not IPA sanctioned
 			[ '(?:\u030a)4', '\u030b' ], // Combining double acute accent
 			[ '(?:\u030a)3', '\u0301' ], // Combining acute accent
@@ -182,53 +182,53 @@
 			[ 'l\\^', 'ˡ' ],
 			// TODO non-IPA superscripts
 
-			[ 'ʽ\\[',          '˞' ],      // [[[
-			[ '\\[\\[',        'ʽ' ],      // [[ // Not IPA sanctioned
-			[ '(?:\u031a)\\]', '‘' ],      // ]]]] // Not IPA sanctioned
-			[ 'ʼ\\]',          '\u031a' ], // ]]]  // Combining left angle above
-			[ '\\]\\]',        'ʼ' ],      // ]]
+			[ 'ʽ\\[', '˞' ], // [[[
+			[ '\\[\\[', 'ʽ' ], // [[ // Not IPA sanctioned
+			[ '(?:\u031a)\\]', '‘' ], // ]]]] // Not IPA sanctioned
+			[ 'ʼ\\]', '\u031a' ], // ]]] // Combining left angle above
+			[ '\\]\\]', 'ʼ' ], // ]]
 
 			[ '(?:\u032f)\\$', '\u0330' ], // $$$ // Combining tilde below
-			[ '(?:\u0329)\\$', '\u032f' ], // $$  // Combining inverted breve below
-			[ '\\$',           '\u0329' ], // $   // Combining vertical line below
+			[ '(?:\u0329)\\$', '\u032f' ], // $$ // Combining inverted breve below
+			[ '\\$', '\u0329' ], // $ // Combining vertical line below
 
 			// TODO non-IPA retroflex hooks
 
-			[ '(?:\u032c)%',   '\u0324' ], // %%% // Combining diaeresis below
-			[ '(?:\u0325)%',   '\u032c' ], // %%  // Combining caron below
-			[ '%', '\u0325' ],             // %   // Combining ring below
+			[ '(?:\u032c)%', '\u0324' ], // %%% // Combining diaeresis below
+			[ '(?:\u0325)%', '\u032c' ], // %% // Combining caron below
+			[ '%', '\u0325' ], // % // Combining ring below
 
 			// TODO non-IPA palatal hooks
 
 			[ '@', '\u030a' ], // Combining ring above
 
-			[ '(?:\u033c)\\{',   '\u0323' ], // {{{{{ // Combining dot below
-			[ '(?:\u033b)\\{',   '\u033c' ], // {{{{  // Combining seagull below
-			[ '(?:\u033a)\\{',   '\u033b' ], // {{{   // Combining square below
-			[ '(?:\u032a)\\{',   '\u033a' ], // {{    // Combining inverted bridge below
-			[ '\\{', '\u032a' ],             // {     // Combining bridge below
+			[ '(?:\u033c)\\{', '\u0323' ], // {{{{{ // Combining dot below
+			[ '(?:\u033b)\\{', '\u033c' ], // {{{{ // Combining seagull below
+			[ '(?:\u033a)\\{', '\u033b' ], // {{{ // Combining square below
+			[ '(?:\u032a)\\{', '\u033a' ], // {{ // Combining inverted bridge below
+			[ '\\{', '\u032a' ], // { // Combining bridge below
 
-			[ '(?:\u0303)~',   '\u0334' ], // ~~    // Combining tilde overlay
-			[ '~',             '\u0303' ], // ~     // Combining tilde
+			[ '(?:\u0303)~', '\u0334' ], // ~~ // Combining tilde overlay
+			[ '~', '\u0303' ], // ~ // Combining tilde
 
-			[ '(?:\u0306)\\*', '\u0307' ], // ****  // Combining dot above
-			[ '(?:\u033d)\\*', '\u0306' ], // ***   // Combining breve
-			[ '(?:\u0308)\\*', '\u033d' ], // **    // Combining x above
-			[ '\\*',           '\u0308' ], // *     // Combining diaeresis
+			[ '(?:\u0306)\\*', '\u0307' ], // **** // Combining dot above
+			[ '(?:\u033d)\\*', '\u0306' ], // *** // Combining breve
+			[ '(?:\u0308)\\*', '\u033d' ], // ** // Combining x above
+			[ '\\*', '\u0308' ], // * // Combining diaeresis
 
 			// TODO Find out what the m with the five asterisks is.
 
-			[ '#&',            '\u0361' ], //          Combining double inverted breve
-			[ '(?:\u030a)&',   '\u035c' ], // @&    // Combining double breve below
+			[ '#&', '\u0361' ], // Combining double inverted breve
+			[ '(?:\u030a)&', '\u035c' ], // @& // Combining double breve below
 
-			[ '(?:\u0318)\\+', '\u0339' ], // ++++  // Combining right half ring below
-			[ '(?:\u0319)_',   '\u031c' ], // ____  // Combining left half ring below
-			[ '(?:\u031d)\\+', '\u0318' ], // +++   // Combining left tack below
-			[ '(?:\u031e)_',   '\u0319' ], // ___   // Combining right tack below
-			[ '(?:\u031f)\\+', '\u031d' ], // ++    // Combining up tack below
-			[ '(?:\u0320)_',   '\u031e' ], // __    // Combining down tack below
-			[ '\\+',           '\u031f' ], // +     // Combining plus sign below
-			[ '_',             '\u0320' ], // _     // Combining minus sign below
+			[ '(?:\u0318)\\+', '\u0339' ], // ++++ // Combining right half ring below
+			[ '(?:\u0319)_', '\u031c' ], // ____ // Combining left half ring below
+			[ '(?:\u031d)\\+', '\u0318' ], // +++ // Combining left tack below
+			[ '(?:\u031e)_', '\u0319' ], // ___ // Combining right tack below
+			[ '(?:\u031f)\\+', '\u031d' ], // ++ // Combining up tack below
+			[ '(?:\u0320)_', '\u031e' ], // __ // Combining down tack below
+			[ '\\+', '\u031f' ], // + // Combining plus sign below
+			[ '_', '\u0320' ], // _ // Combining minus sign below
 
 			[ '=>', '→' ], // Not IPA sanctioned
 			[ 's>', 'σ' ], // Not IPA sanctioned
