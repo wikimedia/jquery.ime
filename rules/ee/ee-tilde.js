b1872c22772a8ead3ed70dbef1e2e23e3d8ac250
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2022-06-15 15:31:40 +0300

    Add the macron sign to the Ewe keyboard
    It may be needed for tones.

--- rules/ee/ee-tilde.js
+++ rules/ee/ee-tilde.js
@@ -9,7 +9,7 @@
 		URL: 'https://github.com/wikimedia/jquery.ime',
 		author: 'Amir E. Aharoni',
 		license: 'GPLv3',
-		version: '1.0',
+		version: '1.1',
 		patterns: [
 			[ '~D', 'Ɖ' ],
 			[ '~d', 'ɖ' ],
@@ -28,6 +28,7 @@
 			[ '~/', '\u0301' ], // Combining acute
 			[ '~\\\\', '\u0300' ], // Combining grave
 			[ '~\\{', '\u0303' ], // Combining tilde
+			[ '~-', '\u0304' ], // Combining macron
 			[ '~\\^', '\u030C' ] // Combining caron
 		]
 	};
