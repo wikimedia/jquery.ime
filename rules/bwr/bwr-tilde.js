ea4093bc42c5d6b4155033413d226e7eba3d667d
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2022-08-14 14:42:51 +0300

    Add ƙ to the Bura keyboard

--- rules/bwr/bwr-tilde.js
+++ rules/bwr/bwr-tilde.js
@@ -5,11 +5,11 @@
 		id: 'bwr-tilde',
 		name: 'Bura tilde',
 		description: 'Bura tilde keyboard',
-		date: '2022-06-11',
+		date: '2022-08-14',
 		URL: 'https://github.com/wikimedia/jquery.ime',
 		author: 'Amir E. Aharoni',
 		license: 'GPLv3',
-		version: '1.0',
+		version: '1.1',
 		maxKeyLength: 2,
 		patterns: [
 			[ '~B', 'Ɓ' ],
@@ -17,7 +17,9 @@
 			[ '~D', 'Ɗ' ],
 			[ '~d', 'ɗ' ],
 			[ '~E', 'Ə' ],
-			[ '~e', 'ə' ]
+			[ '~e', 'ə' ],
+			[ '~K', 'Ƙ' ],
+			[ '~k', 'ƙ' ]
 		]
 	};
 
