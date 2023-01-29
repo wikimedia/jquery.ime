72473d0ef3b93751ba0f41ca6337550b4f344a3b
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2023-01-29 16:03:36 +0200

    Add Dagaare to languages using Burkina-Faso keyboard

--- src/jquery.ime.inputmethods.js
+++ src/jquery.ime.inputmethods.js
@@ -1098,6 +1098,10 @@
 			autonym: 'Deutsch',
 			inputmethods: [ 'de-transliteration' ]
 		},
+		dga: {
+			autonym: 'Dagaare',
+			inputmethods: [ 'mul-bf' ]
+		},
 		din: {
 			autonym: 'Thuɔŋjäŋ',
 			inputmethods: [ 'din-fqsx', 'din-tilde' ]
