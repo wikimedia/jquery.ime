8b8ace423d6a7da17ea3252561fa365d861e1a8e
Benny Lin <bknliem@yahoo.com>
2023-07-30 15:37:57 +0700

    Update btm-transliteration.js
    unicode

--- rules/btm/btm-transliteration.js
+++ rules/btm/btm-transliteration.js
@@ -1,4 +1,4 @@
-( function ( $ ) {
+﻿( function ( $ ) {
 	'use strict';
 
 	var mandailingTransliteration = {
@@ -14,64 +14,64 @@
 		maxKeyLength: 2,
 		patterns: [
 			[ '\\\\([A-Za-z\\>_~\\.0-9])', '\\\\', '$1' ],
-			[ '\u1BF2(a|A)', '', '' ],//default vowel is a, so remove the pangolat
-			[ '\u1BF2(e|E)', '', '\u1BE9' ],//e - 1BE9 \u1BE9 BATAK VOWEL SIGN EE = talinga
-			[ '\u1BF2(i|I)', '', '\u1BEA' ],//i - 1BEA \u1BEA BATAK VOWEL SIGN I = ulua
-			[ '\u1BF2(o|O)', '', '\u1BEC' ],//o - 1BEC \u1BEC BATAK VOWEL SIGN O = siala ulu
-			[ '\u1BF2(u|U)', '', '\u1BEE' ],//u - 1BEE \u1BEE BATAK VOWEL SIGN U = boruta
-			[ '\u1BF2(x|X)', '', '\u1BE7' ], // x - 1BE7 \u1BE7 BATAK VOWEL SIGN E = kebereten
-			[ '\u1BF0(a|A)', '', '\u1BDD' ],//-ng + a = nga
-			[ '\u1BF0(e|E)', '', '\u1BDD\u1BE9' ],//-ng + e = nge
-			[ '\u1BF0(i|I)', '', '\u1BDD\u1BEA' ],//-ng + i = ngi
-			[ '\u1BF0(o|O)', '', '\u1BDD\u1BEC' ],//-ng + o = ngo
-			[ '\u1BF0(u|U)', '', '\u1BDD\u1BEE' ],//-ng + u = ngu
-			[ '\u1BF0(x|X)', '', '\u1BDD\u1BE7' ], //-ng + e = nge (keberetan)
-			[ '\u1BF1(a|A)', '', '\u1BC2' ],//-h + a = ha
-			[ '\u1BF1(e|E)', '', '\u1BC2\u1BE9' ],//-h + e = he
-			[ '\u1BF1(i|I)', '', '\u1BC2\u1BEA' ],//-h + i = hi
-			[ '\u1BF1(o|O)', '', '\u1BC2\u1BEC' ],//-h + o = ho
-			[ '\u1BF1(u|U)', '', '\u1BC2\u1BEE' ],//-h + u = hu
-			[ '\u1BF1(x|X)', '', '\u1BC2\u1BE7' ], //-h + e = he (keberetan)
+			[ '᯲(a|A)', '', '' ],//default vowel is a, so remove the pangolat
+			[ '᯲(e|E)', '', 'ᯩ' ],//e - 1BE9 ᯩ BATAK VOWEL SIGN EE = talinga
+			[ '᯲(i|I)', '', 'ᯪ' ],//i - 1BEA ᯪ BATAK VOWEL SIGN I = ulua
+			[ '᯲(o|O)', '', 'ᯬ' ],//o - 1BEC ᯬ BATAK VOWEL SIGN O = siala ulu
+			[ '᯲(u|U)', '', 'ᯮ' ],//u - 1BEE ᯮ BATAK VOWEL SIGN U = boruta
+			[ '᯲(x|X)', '', 'ᯧ' ], // x - 1BE7 ᯧ BATAK VOWEL SIGN E = kebereten
+			[ 'ᯰ(a|A)', '', 'ᯝ' ],//-ng + a = nga
+			[ 'ᯰ(e|E)', '', 'ᯝᯩ' ],//-ng + e = nge
+			[ 'ᯰ(i|I)', '', 'ᯝᯪ' ],//-ng + i = ngi
+			[ 'ᯰ(o|O)', '', 'ᯝᯬ' ],//-ng + o = ngo
+			[ 'ᯰ(u|U)', '', 'ᯝᯮ' ],//-ng + u = ngu
+			[ 'ᯰ(x|X)', '', 'ᯝᯧ' ], //-ng + e = nge (keberetan)
+			[ 'ᯱ(a|A)', '', 'ᯂ' ],//-h + a = ha
+			[ 'ᯱ(e|E)', '', 'ᯂᯩ' ],//-h + e = he
+			[ 'ᯱ(i|I)', '', 'ᯂᯪ' ],//-h + i = hi
+			[ 'ᯱ(o|O)', '', 'ᯂᯬ' ],//-h + o = ho
+			[ 'ᯱ(u|U)', '', 'ᯂᯮ' ],//-h + u = hu
+			[ 'ᯱ(x|X)', '', 'ᯂᯧ' ], //-h + e = he (keberetan)
 
-			[ '\u1BE4(n|N)', '(i|I)', '\u1BC0\u1BEA\u1BCA\u1BF2' ],//syllable i + ng -> anak ni surat i
-			[ '\u1BE5(n|N)', '(u|U)', '\u1BC0\u1BEE\u1BCA\u1BF2' ],//syllable u + ng -> anak ni surat u
-			[ '\u1BCA\u1BF2(g|G)', '(a|A|e|E|i|I|o|O|u|U|x|X)n', '\u1BF0' ],//1BF0 \u1BF0 BATAK CONSONANT SIGN NG = amisara (-ng)
-			[ '\u1BCA\u1BF2(g|G)', 'n', '\u1BDD\u1BF2' ],//1BDD \u1BDD BATAK LETTER NGA
-			[ '\u1BCA\u1BF2(y|Y)', 'n', '\u1BE0\u1BF2' ],//1BE0 \u1BE0 BATAK LETTER NYA
+			[ 'ᯤ(n|N)', '(i|I)', 'ᯀᯪᯊ᯲' ],//syllable i + ng -> anak ni surat i
+			[ 'ᯥ(n|N)', '(u|U)', 'ᯀᯮᯊ᯲' ],//syllable u + ng -> anak ni surat u
+			[ 'ᯊ᯲(g|G)', '(a|A|e|E|i|I|o|O|u|U|x|X)n', 'ᯰ' ],//1BF0 ᯰ BATAK CONSONANT SIGN NG = amisara (-ng)
+			[ 'ᯊ᯲(g|G)', 'n', 'ᯝ᯲' ],//1BDD ᯝ BATAK LETTER NGA
+			[ 'ᯊ᯲(y|Y)', 'n', 'ᯠ᯲' ],//1BE0 ᯠ BATAK LETTER NYA
 
-			[ '(a|A)', '', '\u1BC0' ],
-			[ '(b|B)', '', '\u1BC5\u1BF2' ],
-			[ '(c|C)', '', '\u1BDA\u1BE6\u1BF2' ],
-			[ '(d|D)', '', '\u1BD1\u1BF2' ],
-			[ '(e|E)', '', '\u1BC0\u1BE9' ],
-			[ '(f|F)', '', '\u1BF3' ],//1BF3 $\u1BF3 BATAK PANONGONAN = virama
-			[ '(g|G)', '', '\u1BCE\u1BF2' ],
-			[ '(h|H)', '(a|e|i|o|u|x)', '\u1BF1' ],//1BF1 \u1BF1 BATAK CONSONANT SIGN H = hajoringan (-h)
-			[ '(h|H)', '', '\u1BC2\u1BF2' ],
-			[ '(i|I)', '', '\u1BE4' ],
-			[ '(j|J)', '', '\u1BD0\u1BF2' ],
-			[ '(k|K)', '', '\u1BC4\u1BE6\u1BF2' ],
-			[ '(l|L)', '', '\u1BDE\u1BF2' ],
-			[ '(m|M)', '', '\u1BD4\u1BF2' ],
-			[ '(n|N)', '', '\u1BCA\u1BF2' ],
-			[ '(o|O)', '', '\u1BC0\u1BEC' ],
-			[ '(p|P)', '', '\u1BC7\u1BF2' ],
-			[ '(q|Q)', '', '\u1BF2' ],//1BF2 $\u1BF2 BATAK PANGOLAT (PANGGOTAP) = virama
-			[ '(r|R)', '', '\u1BD2\u1BF2' ],
-			[ '(s|S)', '', '\u1BDA\u1BF2' ],
-			[ '(t|T)', '', '\u1BD6\u1BF2' ],
-			[ '(u|U)', '', '\u1BE5' ],
-			[ '(v|V)', '', '\u1BE6' ],//1BE6 \u1BE6 BATAK SIGN TOMPI
-			[ '(w|W)', '', '\u1BCB\u1BF2' ],
-			[ '(x|X)', '', '\u1BC0\u1BE7' ],//1BE7 \u1BE7 BATAK VOWEL SIGN E = kebereten
-			[ '(y|Y)', '', '\u1BDB\u1BF2' ],
-			[ '(z|Z)', '', '\u1BF0' ]//1BF0 \u1BF0 BATAK CONSONANT SIGN NG = amisara (-ng)
+			[ '(a|A)', '', 'ᯀ' ],
+			[ '(b|B)', '', 'ᯅ᯲' ],
+			[ '(c|C)', '', 'ᯚ᯦᯲' ],
+			[ '(d|D)', '', 'ᯑ᯲' ],
+			[ '(e|E)', '', 'ᯀᯩ' ],
+			[ '(f|F)', '', '᯳' ],//1BF3 $᯳ BATAK PANONGONAN = virama
+			[ '(g|G)', '', 'ᯎ᯲' ],
+			[ '(h|H)', '(a|e|i|o|u|x)', 'ᯱ' ],//1BF1 ᯱ BATAK CONSONANT SIGN H = hajoringan (-h)
+			[ '(h|H)', '', 'ᯂ᯲' ],
+			[ '(i|I)', '', 'ᯤ' ],
+			[ '(j|J)', '', 'ᯐ᯲' ],
+			[ '(k|K)', '', 'ᯄ᯦᯲' ],
+			[ '(l|L)', '', 'ᯞ᯲' ],
+			[ '(m|M)', '', 'ᯔ᯲' ],
+			[ '(n|N)', '', 'ᯊ᯲' ],
+			[ '(o|O)', '', 'ᯀᯬ' ],
+			[ '(p|P)', '', 'ᯇ᯲' ],
+			[ '(q|Q)', '', '᯲' ],//1BF2 $᯲ BATAK PANGOLAT (PANGGOTAP) = virama
+			[ '(r|R)', '', 'ᯒ᯲' ],
+			[ '(s|S)', '', 'ᯚ᯲' ],
+			[ '(t|T)', '', 'ᯖ᯲' ],
+			[ '(u|U)', '', 'ᯥ' ],
+			[ '(v|V)', '', '᯦' ],//1BE6 ᯦ BATAK SIGN TOMPI
+			[ '(w|W)', '', 'ᯋ᯲' ],
+			[ '(x|X)', '', 'ᯀᯧ' ],//1BE7 ᯧ BATAK VOWEL SIGN E = kebereten
+			[ '(y|Y)', '', 'ᯛ᯲' ],
+			[ '(z|Z)', '', 'ᯰ' ]//1BF0 ᯰ BATAK CONSONANT SIGN NG = amisara (-ng)
 		],
 		patterns_x: [
-			[ '4', '', '\u1BFC' ],
-			[ '5', '', '\u1BFD' ],
-			[ '6', '', '\u1BFE' ],
-			[ '7', '', '\u1BFF' ]
+			[ '4', '', '᯼' ],
+			[ '5', '', '᯽' ],
+			[ '6', '', '᯾' ],
+			[ '7', '', '᯿' ]
 		]
 	};
 
