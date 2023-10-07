395d9d6efe9faed244618d801e9bffd8c55c4636
Benny Lin <bknliem@yahoo.com>
2023-10-07 13:32:18 +0700

    Update jv-keyboard.js
    v 1.01
    - small bugfix: escape special characters
    - change url link

--- rules/jv/jv-keyboard.js
+++ rules/jv/jv-keyboard.js
@@ -5,11 +5,11 @@
 		id: 'jv-keyboard',
 		name: 'Jawa Latin extended keyboard',
 		description: 'Javanese keyboard that include several accented letters',
-		date: '2023-06-06',
-		URL: 'https://github.com/wikimedia/jquery.ime',
+		date: '2023-10-07',
+		URL: 'https://www.mediawiki.org/wiki/Help:Extension:UniversalLanguageSelector/Input_methods/jv-keyboard',
 		author: 'Benny Lin',
 		license: 'GPLv3',
-		version: '1',
+		version: '1.01',
 		patterns: [
 			[ '~E', 'É' ],
 			[ '~e', 'é' ],
@@ -25,10 +25,10 @@
 			[ '~d', 'ḍ' ],
 			[ '~T', 'Ṭ' ],
 			[ '~t', 'ṭ' ],
-			[ '.D', 'Ḍ' ],
-			[ '.d', 'ḍ' ],
-			[ '.T', 'Ṭ' ],
-			[ '.t', 'ṭ' ]
+			[ '\\.D', 'Ḍ' ],
+			[ '\\.d', 'ḍ' ],
+			[ '\\.T', 'Ṭ' ],
+			[ '\\.t', 'ṭ' ]
 		]
 	};
 
