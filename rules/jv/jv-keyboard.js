90d2a815ca8e50519802b1f8b063e0e5efbee239
Benny Lin <bknliem@yahoo.com>
2024-08-28 23:51:18 +0700

    Update jv-keyboard.js
    Add common shortcuts, fix bug, change url link

--- rules/jv/jv-keyboard.js
+++ rules/jv/jv-keyboard.js
@@ -1,22 +1,37 @@
-( function ( $ ) {
+﻿( function ( $ ) {
 	'use strict';
 
 	var jvKeyboard = {
 		id: 'jv-keyboard',
 		name: 'Jawa Latin extended keyboard',
 		description: 'Javanese keyboard that include several accented letters',
-		date: '2023-06-06',
-		URL: 'https://github.com/wikimedia/jquery.ime',
+		date: '2024-06-05',
+		URL: 'https://www.mediawiki.org/wiki/Help:Extension:UniversalLanguageSelector/Input_methods/jv-keyboard',
 		author: 'Benny Lin',
 		license: 'GPLv3',
-		version: '1',
+		version: '1.1',
 		patterns: [
 			[ '~E', 'É' ],
 			[ '~e', 'é' ],
 			[ '`E', 'È' ],
 			[ '`e', 'è' ],
+			[ 'Q', 'È' ],
+			[ 'q', 'è' ],
+			[ 'ÈQ', 'Q' ],
+			[ 'Èq', 'Q' ],
+			[ 'èq', 'q' ],
 			[ '\\^E', 'Ê' ],
 			[ '\\^e', 'ê' ],
+			[ 'X', 'Ê' ],
+			[ 'x', 'ê' ],
+			[ 'ÊX', 'X' ],
+			[ 'Êx', 'X' ],
+			[ 'êx', 'x' ],
+			[ 'E', 'É' ],
+			[ 'e', 'é' ],
+			[ 'ÉE', 'E' ],
+			[ 'Ée', 'E' ],
+			[ 'ée', 'e' ],
 			[ '\\^A', 'Å' ],
 			[ '\\^a', 'å' ],
 			[ '~A', 'Å' ],
@@ -25,10 +40,10 @@
 			[ '~d', 'ḍ' ],
 			[ '~T', 'Ṭ' ],
 			[ '~t', 'ṭ' ],
-			[ '.D', 'Ḍ' ],
-			[ '.d', 'ḍ' ],
-			[ '.T', 'Ṭ' ],
-			[ '.t', 'ṭ' ]
+			[ '\\.D', 'Ḍ' ],
+			[ '\\.d', 'ḍ' ],
+			[ '\\.T', 'Ṭ' ],
+			[ '\\.t', 'ṭ' ]
 		]
 	};
 
