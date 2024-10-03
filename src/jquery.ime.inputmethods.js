5693c9097f57d89fc8d07dfbdb9232e01dfdd5e9
SZTSIIT <69423986+SZTSIIT@users.noreply.github.com>
2024-10-02 20:41:54 -0400

    Requesting PR for Shan Standard Keyboard Layout. (#700)
    * Create my-shn.js
    
    Shan Standard Keyboard per SIIT.
    
    * Update jquery.ime.inputmethods.js
    
    Added Shan Keyboard Layout.
    
    * Update my-shn.js
    
    လွၵ်ႈမိုဝ်းတႆး to  လွၵ်းမိုဝ်းလိၵ်ႈတႆး
    
    * Update jquery.ime.test.fixtures.js
    
    Added for testing Shan Keyboard
    
    * Update my-shn.js
    
    Reordered, cleaned white spaces.
    
    * Update my-shn.js
    
    Corrected note.
    
    ---------
    
    Co-authored-by: SrishAkaTux <ssethi@wikimedia.org>

--- src/jquery.ime.inputmethods.js
+++ src/jquery.ime.inputmethods.js
@@ -718,6 +718,10 @@
 			name: 'မြန်မာဘာသာ xkb',
 			source: 'rules/my/my-xkb.js'
 		},
+		'my-shn': {
+			name: 'လွၵ်းမိုဝ်းလိၵ်ႈတႆး',
+			source: 'rules/my/my-shn.js'
+		},
 		'nb-normforms': {
 			name: 'Normal transliterasjon',
 			source: 'rules/nb/nb-normforms.js'
@@ -1572,7 +1576,11 @@
 		},
 		my: {
 			autonym: 'မြန်မာ',
-			inputmethods: [ 'my-mm3', 'my-xkb' ]
+			inputmethods: [ 'my-mm3', 'my-xkb', 'my-shn' ]
+		},
+		shn: {
+			autonym: 'တႆး',
+			inputmethods: [ 'my-shn' ]
 		},
 		naq: {
 			autonym: 'Khoekhoegowab',
