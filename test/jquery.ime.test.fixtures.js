0e18f3e269fdb05657c7853a7e36aac5dbcb9e6f
ToluAyodele <129562437+ToluAyodele@users.noreply.github.com>
2026-08-21 13:14:33 -0400

    Add test cases for tilde input method

--- test/jquery.ime.test.fixtures.js
+++ test/jquery.ime.test.fixtures.js
@@ -21786,6 +21786,8 @@ var palochkaVariants = {
 		tests: [
 			{ input: '~B', output: 'Ɓ', description: 'Tsishingini tilde ~B -> Ɓ' },
 			{ input: '~b', output: 'ɓ', description: 'Tsishingini tilde ~b -> ɓ' },
+			{ input: '~c', output: 'ɔ', description: 'Tsishingini tilde ~c -> ɔ' },
+			{ input: '~C', output: 'Ɔ', description: 'Tsishingini tilde ~c -> Ɔ' },
 			{ input: '~D', output: 'Ɗ', description: 'Tsishingini tilde ~D -> Ɗ' },
 			{ input: '~d', output: 'ɗ', description: 'Tsishingini tilde ~d -> ɗ' },
 			{ input: 'a~_', output: 'a\u0331', description: 'Tsishingini tilde a~_ -> a̱' },
